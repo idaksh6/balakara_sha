f6e84a54323b09b821012db65337a3bf1b1187c3
idaksh6 <94969215+idaksh6@users.noreply.github.com>
2023-06-12 11:16:45 +0530

    cdn update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,20 +10,18 @@
     <title>STUDENT REGISTRATION PAGE</title>
     <!-- Custom fonts for this template-->
     <link
-        href="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css') }}"
-        rel="stylesheet" type="text/css">
-    <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <link rel="stylesheet" href="{{ asset('/tailwind.css') }}">
+        <link rel="stylesheet" href="{{ asset('/fontawsome.css') }}">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
 
-    <link href="css/app.css'" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/css/sb-admin-2.min.css') }}"
-        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/bootstrap.css') }}">
+
+    <!-- <link href="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/css/sb-admin-2.min.css') }}"
+        rel="stylesheet"> -->
 
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
 </head>
